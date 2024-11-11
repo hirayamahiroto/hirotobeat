f15635d5a151ad3cf55a08603dbd9b80949f511e
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2024-11-12 06:16:57 +0900

    ✨記事の取得件数を上限100に設定

--- src/app/action.ts
+++ src/app/action.ts
@@ -2,7 +2,14 @@ import { client } from "@/utils/microCms/client";
 
 const getBlogs = async () => {
     try {
-        const data = await client.get({ endpoint: "blogs" });          
+        const data = await client.get({ 
+            endpoint: "blogs",
+            queries: { 
+                offset: 0,
+                limit: 100,
+            },
+        });   
+
         return data.contents;
     } catch (error) {
         console.error('Error fetching blogs:', error);
