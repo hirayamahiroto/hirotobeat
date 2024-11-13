55849eefdad8072c2026193e76673b2dec05236e
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2024-11-14 07:10:55 +0900

    使用していないものを削除

--- src/app/api/blog/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-
-export async function GET(request: NextRequest) {
-    return NextResponse.json({
-        message: 'Hello, world!'
-    })
-}
