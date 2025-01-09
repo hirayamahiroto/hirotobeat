139689c6196f26b8bed9a68660461ecb9328b3e8
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-10 00:41:41 +0900

    🔥 詳細ページ画像削除

--- src/components/molecules/blogDetail.tsx
+++ src/components/molecules/blogDetail.tsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import BlogDetailTitle from '../atoms/blogDetailTitle'
-import BlogDetailPublishedAt from '../atoms/blogDetailPublishedAt'
-import BlogDetailCategoryName from '../atoms/blogDetailCategoryName'
-import BlogDetailImage from '../atoms/blogDetailImage'
-import BlogDetailContent from '../atoms/blogDetailContent'
+import React from "react";
+import BlogDetailTitle from "../atoms/blogDetailTitle";
+import BlogDetailPublishedAt from "../atoms/blogDetailPublishedAt";
+import BlogDetailCategoryName from "../atoms/blogDetailCategoryName";
+import BlogDetailContent from "../atoms/blogDetailContent";
 
-
-import { Blog } from '@/types/blog'
+import { Blog } from "@/types/blog";
 
 type BlogDetailProps = {
-    blog: Blog;
-}
+  blog: Pick<Blog, "title" | "publishedAt" | "category" | "eyecatch" | "content">;
+};
 
 const styles = {
-    wrapper: 'max-w-4xl mx-auto px-4 py-16',
-    base: 'bg-white shadow-lg rounded-lg overflow-hidden border border-gray-100"',
-}
+  wrapper: "max-w-4xl mx-auto px-4 py-16",
+  base: 'bg-white shadow-lg rounded-lg overflow-hidden border border-gray-100"',
+};
 
 const BlogDetail = ({ blog }: BlogDetailProps) => {
-    return (
+  return (
     <div className={styles.wrapper}>
-        <article className={styles.base}>
-            <div className="p-8">
-                <BlogDetailTitle title={blog.title} />
-                <BlogDetailPublishedAt publishedAt={blog.publishedAt} />
-                <BlogDetailCategoryName categoryName={blog.category.name} />
-                <BlogDetailImage eyecatchUrl={blog.eyecatch.url}  title={blog.title} />
-                <BlogDetailContent content={blog.content} />
-            </div>
-        </article>
+      <article className={styles.base}>
+        <div className="p-8">
+          <BlogDetailTitle title={blog.title} />
+          <BlogDetailPublishedAt publishedAt={blog.publishedAt} />
+          <BlogDetailCategoryName categoryName={blog.category.name} />
+          <BlogDetailContent content={blog.content} />
+        </div>
+      </article>
     </div>
-)
-}
+  );
+};
 
 export default BlogDetail;
