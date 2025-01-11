5c7ca02c308baa1d4c2912927062cce0348fb219
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-12 02:17:08 +0900

    🔥  middlewareの不要な定義削除

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const allowedOrigins = ["https://hirotobe.vercel.app", "https://localhost:3001"];
   const nonce = crypto.randomUUID();
   const response = NextResponse.next();
 
   response.headers.set(
     "Content-Security-Policy",
     [
+      `default-src 'self'`,
       `script-src 'self' 'unsafe-inline' 'nonce-${nonce}'`,
-      "img-src 'self'",
-      "style-src 'self' 'unsafe-inline'",
-      "connect-src 'self'",
+      `img-src 'self'`,
+      `style-src 'self' 'unsafe-inline'`,
+      `connect-src 'self'`,
     ].join("; ")
   );
 
