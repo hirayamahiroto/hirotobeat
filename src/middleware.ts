a7ec9879521a4ca45258a399eba01a0b3e4c8ec8
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-12 02:28:44 +0900

    🔥  middlewareの不要な定義削除

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,8 @@ export function middleware() {
   response.headers.set(
     "Content-Security-Policy",
     [
-      `default-src 'self'`,
       `script-src 'self' 'unsafe-inline' 'nonce-${nonce}'`,
+      `img-src 'self'`,
       `style-src 'self' 'unsafe-inline'`,
       `connect-src 'self'`,
     ].join("; ")
